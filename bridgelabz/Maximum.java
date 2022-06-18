51f73331667ec77e92561c268678ee9b78865d47
sravan <sravan.k.vodnala@gmail.com>
2022-06-18 12:31:27 +0530

    Added: find integer maximum

--- bridgelabz/Maximum.java
+++ bridgelabz/Maximum.java
@@ -1,10 +1,19 @@
 package com.bridgelabz;
 
 public class Maximum {
+    public static Integer testCase(Integer a, Integer b, Integer c) {
+        Integer max = a;
+        if (b.compareTo(max) > 0) {
+            max = b;
+        }
+        if (c.compareTo(max) > 0) {
+            max = c;
+        }
+        return max;
+    }
 
     public static void main(String[] args) {
-        System.out.println("Welcome to generics program");
-
+        System.out.println("Welcome Generics program to find maximum");
+        System.out.println("Maximum Of Three Integers is :" + testCase(2, 8, 9));
     }
 }
-
