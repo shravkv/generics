ee7acb5ddcd6f19228adafb550ffb915c186ed0d
sravan <sravan.k.vodnala@gmail.com>
2022-06-18 22:55:52 +0530

    Added: Extend max method to print max using Generic

--- bridgelabz/Maximum.java
+++ bridgelabz/Maximum.java
@@ -2,17 +2,26 @@ package com.bridgelabz;
 
 import java.util.Arrays;
 
-public class Maximum {
+public class Maximum<T> {
 
-        public static <T> T testCase(T... a) {
-            Arrays.sort(a);
-            return a[a.length - 1];
-        }
-
-        public static void main(String[] args) {
-            System.out.println("Welcome Generics program to find maximum");
-            System.out.println("Maximum of Integer values is :" + testCase(2, 6, 9, 105,4, 9, 25, 59, 94));
-            System.out.println("Maximum of Float Values is :" + testCase(2.4f, 5.9f, 2.8f, 8.6f, 10.7f));
-            System.out.println("Maximum of String values is :" + testCase("Sony", "Bird", "June", "Dog", "Egg", "Fox", "Grill"));
-        }
+    public static void main(String[] args) {
+        System.out.println("Welcome Generics program to find maximum");
+        Maximum<Integer> integerMaximum = new Maximum<>();
+        Maximum<Float> floatMaximum = new Maximum<>();
+        Maximum<String> stringMaximum = new Maximum<>();
+        integerMaximum.testCase(10, 8, 4, 23, 21);
+        floatMaximum.testCase(2.4f, 3.8f, 6.8f, 8.2f, 1.6f);
+        stringMaximum.testCase("Apple", "May", "Juice", "King", "Cat", "Rice");
     }
+
+    public T testCase(T... a) {
+        Arrays.sort(a);
+        T max = a[a.length - 1];
+        printMax(max);
+        return max;
+    }
+
+    private void printMax(T max) {
+        System.out.println(max);
+    }
+}
