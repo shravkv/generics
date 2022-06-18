73cfdbdbc71de8373e93d73c75c949c53e2e41d5
sravan <sravan.k.vodnala@gmail.com>
2022-06-18 22:16:18 +0530

    Added: find string maximum of three strings

--- bridgelabz/Maximum.java
+++ bridgelabz/Maximum.java
@@ -1,8 +1,8 @@
 package com.bridgelabz;
 
 public class Maximum {
-    public static <T extends Comparable> T testCase(T a, T b, T c) {
-        T max = a;
+    public static String testCase(String a, String b, String c) {
+        String max = a;
         if (b.compareTo(max) > 0) {
             max = b;
         }
@@ -14,8 +14,6 @@ public class Maximum {
 
     public static void main(String[] args) {
         System.out.println("Welcome to Generics program to find maximum");
-        System.out.println("Maximum Of Three Integers is :" + testCase(2, 8, 9));
-        System.out.println("Maximum Of Three Floats is :" + testCase(2.4f, 8.5f, 5.9f));
         System.out.println("Maximum Of Three Strings is :" + testCase("Nokia", "Samsung", "Oneplus"));
 
     }
